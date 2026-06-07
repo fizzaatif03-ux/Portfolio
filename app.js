2b98aa352f08b208701eeaab8f21f61523f76ee2
fizzaatif03-ux <fizzaatif03@gmail.com>
2026-06-07 18:26:13 +0500

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const hamburger =document.querySelector('#hamburger');
 const menu =document.querySelector('#menu');
-
+const hLink =document.querySelectorAll('#hLink');
 hamburger.addEventListener("click",()=>{
     menu.classList.toggle("hidden");
     // menu.classList.toggle("bg-white");
 });
+
+hLink.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.add("hidden");
+    });
+});
+
